2df0e6c86c04b519b320a519989a1cad892a8ecf
Lucia Lučanová <lucia.lucanova2@gmail.com>
2024-10-13 20:49:15 +0200

    homework- locators

---
+++ src/tests/homework.spec.js
@@ -0,0 +1,33 @@
+import { test } from "@playwright/test";
+
+test("locators", async ({ page }) => {
+    await page.goto("https://team8-2022brno.herokuapp.com/registrace");
+        //Meno a priezvisko: 
+        await page.locator ("#name");
+        await page.locator (".form - control");
+        await page.locator ('[name="name"]');
+        await page.getByText("Jméno a příjmení");
+
+        //Políčko pre email
+        await page.locator("#email");
+        await page.locator ('.form - control');
+        await page.locator ('[type = "email"]');
+        await page.getByText('Email');
+
+        //Políčko pre heslo
+        await page.locator("#password");
+        await page.locator ('[type="password"]');
+        await page.getByText("Heslo");
+        
+        //kontrola hesla
+        await page.locator("#password - confirm");
+        await page.locator ('.form-control');
+        await page.getByText("Kontrola hesla")
+
+        //tlačítko pre registráciu
+        await page.locator('.register-button');
+        await page.locator('[type="submit"]');
+
+});
+
+        
